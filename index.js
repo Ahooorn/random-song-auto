402534e22de4d22a1939e054cf14791d3316e6f0
Ahooorn <elisohn1888@gmail.com>
2026-04-27 13:01:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ const playlist = [
   "https://open.spotify.com/track/6TWbY1dq8eYtFiMiGdBlOa",
   "https://open.spotify.com/track/2libVw5xnVwelZ69K47bDz",
   "https://open.spotify.com/track/0ABFNHwvoIm3VzPbpLPyuN",
+  "https://open.spotify.com/track/63iPYTdJKfwTo2YDWKHOqr"
 ];
 
 // 🕒 Jeden Tag um 9:00 österreichische Zeit (= 7:00 UTC im Sommer)
