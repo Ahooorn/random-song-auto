b670956837cc8b0f2f479f7fdcd7eeab73e87c31
Ahooorn <elisohn1888@gmail.com>
2026-04-27 11:18:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function getRandomTrack() {
 }
 
 // 🕒 Jeden Tag um 9:00 österreichische Zeit (= 7:00 UTC im Sommer)
-cron.schedule("02 9 * * *", async () => {
+cron.schedule("20 9 * * *", async () => {
   try {
     const channel = await client.channels.fetch(CHANNEL_ID);
     const song = await getRandomTrack();
